6b5689e62d52e35c3d97ca6f8b171260b7b9af71
jauntyjay09 <49561283+jauntyjay09@users.noreply.github.com>
2020-11-18 00:44:08 +0530

    Update ppmail.php

--- ppmail.php
+++ ppmail.php
@@ -1,17 +1,50 @@
-<?PHP
-$sender = 'someone@somedomain.in';
-$recipient = 'akeshav53@gmail.com';
 
-$subject = "php mail test";
-$message = "php test message";
-$headers = 'From:' . $sender;
+<?php
+// Multiple recipients
+$to = 'jayakumar2001mhk@gmail.com'; // note the comma
 
-if (mail($recipient, $subject, $message, $headers))
-{
-    echo "Message accepted";
+// Subject
+$subject = 'Birthday Reminders for August';
+
+// Message
+$message = '
+<html>
+<head>
+  <title>Birthday Reminders for August</title>
+</head>
+<body>
+  <p>Here are the birthdays upcoming in August!</p>
+  <table>
+    <tr>
+      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+    </tr>
+    <tr>
+      <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
+    </tr>
+    <tr>
+      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+    </tr>
+  </table>
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers[] = 'MIME-Version: 1.0';
+$headers[] = 'Content-type: text/html; charset=iso-8859-1';
+
+// Additional headers
+$headers[] = 'To: Mary <mary@example.com>, Kelly <kelly@example.com>';
+$headers[] = 'From: Birthday Reminder <birthday@example.com>';
+$headers[] = 'Cc: birthdayarchive@example.com';
+$headers[] = 'Bcc: birthdaycheck@example.com';
+
+// Mail it
+$rtp=mail($to, $subject, $message, implode("\r\n", $headers));
+if($rtp){
+    echo "hii";
 }
-else
-{
-    echo "Error: Message not accepted";
+else{
+    echo "try ";
 }
 ?>
