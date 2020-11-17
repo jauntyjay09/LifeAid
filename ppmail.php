e99ce4e911e8f1d3ead4cb5a4f949ebf8e5f440e
jauntyjay09 <49561283+jauntyjay09@users.noreply.github.com>
2020-11-18 00:12:36 +0530

    Update ppmail.php

--- ppmail.php
+++ ppmail.php
@@ -1,31 +1,41 @@
 <?php
+$to = "jayakumar2001mhk@gmail.com, somebodyelse@example.com";
+$subject = "HTML email";
 
-$email="jayakumar.18bce7031@vitap.ac.in";
-$subject = " Signup ";
-$txt = "
-
-
-hiiiii 
-
-
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+</tr>
+<tr>
+<td>John</td>
+<td>Doe</td>
+</tr>
+</table>
+</body>
+</html>
 ";
-//$headers = "MIME-Version: 1.0" . "\r\n";
-//$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: somebodyelse@example.com";
 
-$rtp=mail($email,$subject,$txt,$headers);
-          if($rtp==true){
-             $msg = "User Created ccccccSuccessfully, mail sent ";
-          }
-          else{
-              $msg = "User , mail NOT sent error";
-          }
-          echo $msg;
-          
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
-          
+// More headers
+$headers .= 'From: <webmaster@example.com>' . "\r\n";
+$headers .= 'Cc: myboss@example.com' . "\r\n";
 
-?>
-
-
+$rtp=mail($to,$subject,$message,$headers);
+if($rtp){
+          echo "good";
+}
+else{
+          echo "try";
+}
+?> 
