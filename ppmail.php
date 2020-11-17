8ee1aba7ece5976e97cbffe198feb7bdf9e64a72
jauntyjay09 <49561283+jauntyjay09@users.noreply.github.com>
2020-11-18 00:15:08 +0530

    Update ppmail.php

--- ppmail.php
+++ ppmail.php
@@ -1,41 +1,15 @@
 <?php
-$to = "jayakumar2001mhk@gmail.com, somebodyelse@example.com";
-$subject = "HTML email";
+$to = "mhkjayakumar@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
 
-$message = "
-<html>
-<head>
-<title>HTML email</title>
-</head>
-<body>
-<p>This email contains HTML Tags!</p>
-<table>
-<tr>
-<th>Firstname</th>
-<th>Lastname</th>
-</tr>
-<tr>
-<td>John</td>
-<td>Doe</td>
-</tr>
-</table>
-</body>
-</html>
-";
-
-// Always set content-type when sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// More headers
-$headers .= 'From: <webmaster@example.com>' . "\r\n";
-$headers .= 'Cc: myboss@example.com' . "\r\n";
-
-$rtp=mail($to,$subject,$message,$headers);
+$rtp=mail($to,$subject,$txt,$headers);
 if($rtp){
-          echo "good";
+          echo "goodw";
 }
 else{
-          echo "try";
+          echo "trys";
 }
 ?> 
