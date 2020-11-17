4f4509378b8a3d220773ad0d7d9d3972186d6570
jauntyjay09 <49561283+jauntyjay09@users.noreply.github.com>
2020-11-18 00:21:40 +0530

    Update ppmail.php

--- ppmail.php
+++ ppmail.php
@@ -1,15 +1,68 @@
 <?php
-$to = "mhkjayakumar@gmail.com";
-$subject = "My subject";
-$txt = "Hello world!";
-$headers = "From: webmaster@example.com" . "\r\n" .
-"CC: somebodyelse@example.com";
 
-$rtp=mail($to,$subject,$txt,$headers);
+        $subject = "Your Payment is Successful";
+        $msg= "
+    ​
+        <!DOCTYPE html>
+        <html lang='en' dir='ltr'>
+          <head>
+            <meta charset='utf-8'>
+            <title></title>
+            <style>
+            @media print {
+              .print_hidden{display: none;}
+            }
+              table{width: 600px; border: solid 1px; padding: 24px;margin: auto;}
+              .bottom_solid{border-top: solid 1px; height: 30px}
+              .right{text-align: right;}
+              .dashed_bottom{border-bottom: dashed 1px;}
+            </style>
+          </head>
+          <body>
+          
+        <div class='print_hidden'>
+    ​
+    ​
+    ​
+    ​
+    ​
+            
+          <h4> Dear <b>jai/b>, </h4>
+    <p>This is an acknowledgement mail from Protrainy that your application form with Enrolment no. <b>rrr</b> and Successful Order ID <b>hop</b> for the
+    IndustryReady Certification Course October 2020 has been Successfully Completed and Submitted . Please keep this receipt for future reference.
+    </p>
+
+    ​
+            Acknowledgement Receipt
+        
+    ​
+       
+    ​
+        
+       </body>
+       </html>
+    ​
+        ";
+    
+   
+    ​
+    
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+    $headers .= 'From: Protrainy <no_reply@protrainy.com>' . "\r\n";
+   
+    ​
+   $rtp= mail("$emaily","$subject",$msg,$headers);
+
+
+
+
+
+
 if($rtp){
-          echo "goodw";
+          echo "good";
 }
 else{
-          echo "trys";
+          echo "try";
 }
 ?> 
