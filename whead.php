3c34d5144bdbc64aff915b5c610fd236f7907c04
jauntyjay09 <49561283+jauntyjay09@users.noreply.github.com>
2020-11-19 02:33:53 +0530

    Create whead.php

---
+++ whead.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light " style="background-color: white;">
+        <a class="navbar-brand" href="wel.html">
+            <img src="rlogo.png" width="120" height="100" class="d-inline-block align-top" alt="">
+   </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            
+            
+          </ul>
+          <ul class="nav ">
+            <li class="nav-item">
+              <a class="nav-link active"  href="d.html"><h5>Donate</h5></a>
+            </li>
+            
+          </ul>
+          <ul class="nav ">
+            <li class="nav-item">
+                <a class="nav-link active" href="s.html"><h5>Request</h5></a>
+          
+            </li>
+            
+          </ul>
+          <ul class="nav ">
+            <li class="nav-item">
+              <a class="nav-link active" href="support.html"><h5>Support</h5></a>
+            </li>
+            
+          </ul>
+          
+          
+        </div>
+      </nav>
+  
+    
+
+</body>
+</html>
