fa6504f492db95ed4566795db1bba9f14579a596
jauntyjay09 <49561283+jauntyjay09@users.noreply.github.com>
2020-11-19 05:10:57 +0530

    Create creddb.php

---
+++ warmin/creddb.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "aafx13fb6peysz.c7gw781jqglu.us-west-2.rds.amazonaws.com";
+$username = "jaysvasth98";
+$password = "svasthjay98";
+$dbname = "ebdb";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
