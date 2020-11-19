0a9c0005c384f5db15df414be240afb7b823af3b
jauntyjay09 <49561283+jauntyjay09@users.noreply.github.com>
2020-11-19 05:47:33 +0530

    Update dt.php

--- warmin/dt.php
+++ warmin/dt.php
@@ -109,7 +109,7 @@ if(!empty($_GET['state'])&& !empty($_GET['bg'])){
                 <div class="card mx-auto bg-success " >
                     <div class="card-header mx-auto">
                       Records
-                        <?php echo $msg; ?>
+                        
                     </div>
                     <div class="card" >
                         <table class="table">
